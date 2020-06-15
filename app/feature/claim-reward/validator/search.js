e371547d0a91b2903ada4f68205ea483ceee6fa4
Hung Tran <hungtv@blockchainlabs.asia>
2020-06-15 17:29:39 +0700

    Allow currency_symbol null

--- app/feature/claim-reward/validator/search.js
+++ app/feature/claim-reward/validator/search.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  currency_symbol: Joi.string().max(64).required(),
+  currency_symbol: Joi.string().max(100).allow('').allow(null).optional(),
   ext_client_id: Joi.string().max(250).required(),
   offset: Joi.number().min(0).required(),
   limit: Joi.number().greater(0).required(),
