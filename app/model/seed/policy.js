4a4187393de8f6d4bc16710b611cde46ee4e9ae9
Hung Tran <hungtv@blockchainlabs.asia>
2020-06-30 11:36:57 +0700

    Seed policies data

--- app/model/seed/policy.js
+++ app/model/seed/policy.js
@@ -15,38 +15,41 @@ module.exports = async () => {
 
     models.push(new MembershipPolicy({
       name: 'AffiliateSystem - Membership Policy',
-      proportion_share: 10,
-      max_levels: 4,
+      proportion_share: 1,
       membership_rate: {
-        'SILVER': 20,
-        'GOLD': 50,
-        'PLATINUM': 70,
-        'DIAMOND': 100,
-      }
+        // Silver
+        'd146bc01-9e56-4664-9788-79e518877f0b': 20,
+        // Gold
+        '88fda933-0658-49c4-a9c7-4c0021e9a071': 100,
+      },
+      is_membership_system: false,
     }));
 
     models.push(new MembershipAffiliatePolicy({
       name: 'AffiliateSystem - Membership Affiliate Policy',
-      proportion_share: 20,
+      proportion_share: 0,
       max_levels: 4,
       rates: [50, 30, 15, 5],
       organization_id: '69366383-b9c2-497c-1111-391b017772ba',
+      is_membership_system: false,
     }));
 
     models.push(new AffiliatePolicy({
       name: 'AffiliateSystem - Affiliate Policy',
       max_levels: null,
-      proportion_share: 20,
-      rates: [50, 30, 15, 5],
+      proportion_share: 10,
+      rates: [50, 25, 15, 10],
       organization_id: '69366383-b9c2-497c-1111-391b017772ba',
+      is_membership_system: false,
     }));
 
     models.push(new AffiliatePolicy({
       name: 'MembershipSystem - Affiliate Policy',
-      max_levels: 5,
-      rates: [50, 30, 15, 5],
-      proportion_share: 30,
+      max_levels: 2,
+      rates: [75, 25],
+      proportion_share: 20,
       organization_id: '69366383-b9c2-497c-1111-391b017772ba',
+      is_membership_system: true,
     }));
 
     await Model.bulkCreate(models, {
